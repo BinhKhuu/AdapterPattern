c02a4bc7c6a5c511be0f77dad3fa36e039ef01ce
binhk <binhkhuu90@gmail.com>
2024-01-13 15:00:02 +0800

    light refactor

--- ConsoleApp1/Services/BooksService.cs
+++ ConsoleApp1/Services/BooksService.cs
@@ -23,6 +23,16 @@ namespace ConsoleApp1.Services
         {
             var requestData = XElement.Parse(_booksDataService.GetBooksXML());
             var libraryBooksRequest = XElement.Parse(_libraryServiceXMLAdapter.GetBooks());
+            var books = ParseBooks(requestData);
+
+            var libraryBooks = ParseBooks(libraryBooksRequest);
+
+            var allBooks = books.Concat(libraryBooks).ToList();
+            return allBooks;
+        }
+
+        private List<Books> ParseBooks(XElement requestData)
+        {
             var books = requestData.Elements("book").Select(b => new Books
             {
                 Author = b.Element("author") is null ? string.Empty : b.Element("author").Value,
@@ -34,19 +44,7 @@ namespace ConsoleApp1.Services
             })
             .ToList();
 
-            var libraryBooks = libraryBooksRequest.Elements("book").Select(b => new Books
-            {
-                Author = b.Element("author") is null ? string.Empty : b.Element("author").Value,
-                Title = b.Element("title") is null ? string.Empty : b.Element("title").Value,
-                Genre = b.Element("genre") is null ? string.Empty : b.Element("genre").Value,
-                Price = b.Element("price") is null ? 0 : (double)b.Element("price"),
-                PublishDate = b.Element("publish_date") is null ? null : DateOnly.Parse(b.Element("publish_date").Value),
-                Description = b.Element("description") is null ? string.Empty : b.Element("description").Value,
-            })
-            .ToList();
-
-            var allBooks = books.Concat(libraryBooks).ToList();
-            return allBooks;
+            return books;
         }
     }
 }
